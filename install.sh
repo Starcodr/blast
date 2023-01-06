e91bcf82a34d364bcb5afc6e707eb3a6d1be9de1
Lars Boris <larsboris@gmail.com>
2023-01-06 18:23:45 +0100

    New install procedure test.

--- install.sh
+++ install.sh
@@ -3,10 +3,3 @@ touch $HOME/.config/blast/bookmarks.json
 touch $HOME/.config/blast/latest_history.txt
 touch $HOME/.config/blast/command.sh
 chmod +x $HOME/.config/blast/command.sh
-
-mkdir -p $HOME/bin
-cp bin/blast $HOME/bin
-chmod +x $HOME/bin/blast
-
-npm install
-npm install -g .
